f850a506f09da099cdea865129c81bd30fd51e8d
passeggero23 <passeggero23@gmail.com>
2026-02-19 09:16:35 +0100

    init: implementation of DashboardPage
    Create basic DashboardPage widget.
    - Set up UI placeholder for analytics view.
    - Align with Apache 2.0 headers.

---
+++ lib/pages/dashboard_page.dart
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2026 Passeggero23
+ * Licensed under the Apache License, Version 2.0
+ */
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatelessWidget {
+  const DashboardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Dashboard',
+          style: TextStyle(fontSize: 22),
+        ),
+      ),
+    );
+  }
+}
