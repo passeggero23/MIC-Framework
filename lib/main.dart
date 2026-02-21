17ca49420266220c7a20d9ad1ecf96f23af826de
passeggero23 <passeggero23@gmail.com>
2026-02-21 07:58:16 +0100

    /*  * Copyright 2026 MIC-Framework  * Licensed under the Apache License, Version 2.0  */  import 'package:flutter/material.dart'; import 'main_navigation.dart';  void main() {   runApp(const MICFramework()); }  class MICFramework extends StatelessWidget {   const MICFramework({super.key});    @override   Widget build(BuildContext context) {     return MaterialApp(       title: 'MIC Framework',       debugShowCheckedModeBanner: false,       theme: ThemeData(         brightness: Brightness.dark,         primarySwatch: Colors.blue,         useMaterial3: true,       ),       home: const MainNavigation(),     );   } }
    Eseguita la pulizia finale del file main.dart. Rimossi gli import inutilizzati (material e home_page) che causavano il fallimento del check di analisi statica. Ora il flusso di avvio è delegato correttamente al modulo MainNavigation, garantendo una build pulita e conforme su GitHub Actions.

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,29 @@
+/*
+ * Copyright 2026 MIC-Framework
+ * Licensed under the Apache License, Version 2.0
+ */
+
 import 'package:flutter/material.dart';
-import 'pages/home_page.dart';
+import 'main_navigation.dart';
+
+void main() {
+  runApp(const MICFramework());
+}
+
+class MICFramework extends StatelessWidget {
+  const MICFramework({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MIC Framework',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const MainNavigation(),
+    );
+  }
+}
