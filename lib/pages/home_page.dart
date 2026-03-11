965a0b251e1d5a5ed2db68e80a3f8d0f5c713df4
passeggero23 <passeggero23@gmail.com>
2026-03-11 09:44:54 +0100

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,7 +7,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('Home MIC Framework'),
+        child: Text('MIC APP - HOME'),
       ),
     );
   }
