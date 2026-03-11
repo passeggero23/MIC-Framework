c2cfb3197640df387f7fb0d66ef2340933f5ad43
passeggero23 <passeggero23@gmail.com>
2026-03-11 20:38:51 +0100

    Update main_navigation.dart

--- lib/main_navigation.dart
+++ lib/main_navigation.dart
@@ -1,3 +1,43 @@
+import 'package:flutter/material.dart';
+// Controlla che questi file esistano fisicamente nella cartella lib/
 import 'package:mic_app/pages/home_page.dart';
 import 'package:mic_app/features/dashboard/presentation/dashboard_page.dart';
-import 'package:mic_app/pages/settings_page.dart'
+import 'package:mic_app/pages/settings_page.dart';
+
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int _selectedIndex = 0;
+
+  // Se i file sopra hanno la X rossa, anche questi daranno errore
+  final List<Widget> _pages = [
+    const HomePage(),
+    const DashboardPage(),
+    const SettingsPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.dashboard), label: 'Dashboard'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
+        ],
+      ),
+    );
+  }
+}
